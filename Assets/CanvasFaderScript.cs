e719b647cb344ea03fb5c87ea97c109c2d9614fa
paulkruest <46826261+paulkruest@users.noreply.github.com>
2019-01-19 13:03:18 -0800

    Initial commit

---
+++ Assets/CanvasFaderScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/*
+ * A Simple Canvas Fader Script (Unity 5.6)
+ * 
+ * 
+ * Uses : Fade out or Fade In a UI Canvas. Not for transitioning a whole Scene with elements. 
+ * 
+ * Usage : Add a Canvas Group Component to the UI Canvas. Add this script to the canvas. Add the function for onComplete.
+ * 
+ * modified : 15/5/2017
+ * by Monster Brain Games (monsterbraininc.com)
+ * 
+ */
+
+public class CanvasFaderScript : MonoBehaviour
+{
+    enum eFadeType
+    {
+        fade_in,
+        fade_out
+    };
+
+    private float _alpha = 1.0f;
+
+    private bool _isFading = false;
+
+    [SerializeField]
+    private eFadeType fadeType = eFadeType.fade_in;
+    [SerializeField]
+    private float fadingDuration = 1.0f;    //Time for fading
+    private float fadeSpeed = 1.0f;
+
+    [SerializeField]
+    private bool _startFadeOnAwake = false;
+
+    [SerializeField]
+    private UnityEvent onCompleteEvent;
+
+    private CanvasGroup _canvasGroup;
+
+    void Awake()
+    {
+        if (fadeType == eFadeType.fade_in)
+            _alpha = 0;
+        else if (fadeType == eFadeType.fade_out)
+            _alpha = 1.0f;
+
+
+        _canvasGroup = GetComponent<CanvasGroup>();
+        if (_canvasGroup == null)
+            Debug.LogError("[warning] CanvasFadeScript: please add a Canvas Group to the Canvas");
+
+        if (fadingDuration > 0)
+            fadeSpeed = 1 / fadingDuration;
+
+        if (_startFadeOnAwake)
+            StartFading();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void StartFading()
+    {
+        setCanvasAlpha();
+        _isFading = true;
+    }
+
+    void setCanvasAlpha()
+    {
+        if (_canvasGroup != null) _canvasGroup.alpha = _alpha;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isFading)
+        {
+            if (fadeType == eFadeType.fade_in)
+            {
+                _alpha += Time.deltaTime * fadeSpeed;
+                if (_alpha > 0.95f)
+                    onFadeCompleted();
+            }
+            else if (fadeType == eFadeType.fade_out)
+            {
+                _alpha -= Time.deltaTime * fadeSpeed;
+                if (_alpha < 0.05f)
+                    onFadeCompleted();
+            }
+
+            setCanvasAlpha();
+        }
+    }
+
+    public void onFadeCompleted()
+    {
+        _isFading = false;
+
+        //Call On Complete Here
+        if (onCompleteEvent != null)
+            onCompleteEvent.Invoke();
+    }
+}
---
+++ Assets/Scripts/FadeTransitionScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeTransitionScript : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update ()
+    {
+      if (Input.GetMouseButtonDown(0)) //This is the trigger to transition to fade out and next scene
+        {
+            FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel ()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
