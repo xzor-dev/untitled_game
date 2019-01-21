9b97355eac4eb1efc5b42ad67d6cdc43dbab567b
paulkruest <46826261+paulkruest@users.noreply.github.com>
2019-01-20 22:33:07 -0800

    Player Movement
    1st round

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float rotateSpeed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private int jumps;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        if (moveDirection.magnitude > 1)
+            moveDirection = moveDirection / moveDirection.magnitude;
+
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetKeyDown("space"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
